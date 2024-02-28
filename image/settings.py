3197cbbf6ed98d9459a2615c54d681cc187858eb
Meet Bhalodi <meetbhalodi@Meets-MacBook-Air.local>
2024-02-28 09:20:59 +0530

    Installed psycopg2 and psycopgs-binary

--- image/settings.py
+++ image/settings.py
@@ -54,15 +54,13 @@ TEMPLATES = [
 WSGI_APPLICATION = 'image.wsgi.application'
 
 DATABASES = {
-    'default': {
-         "default": {
+    "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": config('DATABASE_NAME'),
         "USER": config('DATABASE_USER'),
         "PASSWORD": config('DATABASE_PASSWORD'),
         "HOST": config('DATABASE_HOST'),
         "PORT": config('DATABASE_PORT'),
-    }
     }
 }
 
